5aeaa5eccdfde412ebcb3717676419babdbedc20
AaHui <cmh810828@gmail.com>
2017-10-29 04:49:39 +0800

    [C#]Gif

---
+++ CSharp_Gif_test_171029.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Resources;
+
+namespace WindowsFormsApp1
+{
+    public partial class CSharp_Gif_test_171029 : Form
+    {
+        public CSharp_Gif_test_171029()
+        {
+            InitializeComponent();
+        }
+    }
+}
